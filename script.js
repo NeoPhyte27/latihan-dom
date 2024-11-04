e43186d4794817671a1184da1a6bd87a758b980b
NeoPhyte27 <aldijinkpro27@gmail.com>
2024-11-04 16:25:53 +0700

    membuat menu search di form

--- script.js
+++ script.js
@@ -20,5 +20,31 @@ items.forEach(function (item) {
             </div>
         </div>
         `;
-        document.getElementById('listBarang').innerHTML += card;
+  document.querySelector('#listBarang').innerHTML += card;
+});
+
+// info : membuat event search untuk form
+document.querySelector('#formItem').addEventListener('submit', function (event) {
+  event.preventDefault();
+  var keyword = document.querySelector('#keyword').value;
+  var filteredItems = items.filter(function (item) {
+    return item[1].toLowerCase().includes(keyword.toLowerCase());
+  });
+  document.querySelector('#listBarang').innerHTML = '';
+  filteredItems.forEach(function (item) {
+    var card = `
+        <div class ="col-4 mt-2">
+            <div class="card" style="width: 18rem;">
+                <img src="${item[4]}" class="card-img-top" height="200px" width="200px" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title" id="itemName">${item[1]}</h5>
+                    <p class="card-text" id="itemDesc">${item[3]}</p>
+                    <p class="card-text">Rp ${item[2]}</p>
+                    <a href="#" class="btn btn-primary" id="addCart">Tambahkan ke keranjang</a>
+                </div>
+            </div>
+        </div>
+        `;
+    document.querySelector('#listBarang').innerHTML += card;
+  });
 });
